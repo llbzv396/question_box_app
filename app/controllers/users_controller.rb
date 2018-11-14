186b749cc9a576b387255b9b3a9282e0ad3077ab
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-11-15 01:23:35 +0900

    Add login & signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def new
+    @user = "Sample"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get '/contact',   to: 'top_pages#contact'
 
   get '/signup',    to: 'users#new'
+  get '/login',     to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
