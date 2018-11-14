43cd00a2558c9589863ddf1617012bb7897f920a
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-11-15 01:23:35 +0900

    Modified views

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,6 @@
 class TopPagesController < ApplicationController
   def home
+    @user = User.find_by(id: session[:user_id])
     @micropost = current_user.microposts.build if logged_in?
     @microposts = Micropost.all.includes(:user)
   end
