b487807ece791826fbd26d6ae8a3c8336a7a4fd6
Kyosuke Awata <llbzv396@ybb.ne.jp>
2018-11-15 01:23:35 +0900

    Add Users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show]
+  before_action :logged_in_user, only: [:index, :show]
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
